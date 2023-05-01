b2b1367e192f064e53c2cb26ed42497ec830bcaf
spcover <98606933+spcover@users.noreply.github.com>
2023-04-30 19:48:24 -0500

    Update StartBall.cs to add ball tracing feature

--- Assets/StartBall.cs
+++ Assets/StartBall.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StartBall : MonoBehaviour
 {
 	float passed;
+	float total;
+	float limit = 180f;
+	static string[] balls = new string[]{"DirtBall", "CopperBall", "MarbleBall", "WaterBall", "SoccerBall", "TennisBall", "VolleyBall"};
+	static string[] trace = new string[balls.Length];
+	static int index = 0;
+
     // Start is called before the first frame update
     void Start()
     {	
 		passed = 0.0f;
+		total = 0.0f;
 		AddForce(500f);
     }
 
@@ -17,49 +25,86 @@ public class StartBall : MonoBehaviour
 		Vector3 direction = new Vector3(Mathf.Sin(angle*Mathf.PI), 0f, Mathf.Cos(angle*Mathf.PI));
         Rigidbody m_Rigidbody = GetComponent<Rigidbody>();
 		m_Rigidbody.AddForce(force * direction);
-		Debug.Log(gameObject.name + ":" + force * direction);
+//		Debug.Log(gameObject.name + ":" + force * direction);
 	}
 
     // Update is called once per frame
     void Update()
     {
 		passed = passed + Time.deltaTime;
-		if(passed > 5f){
+		if(passed > 5f && total < limit){
 			AddForce(200f);
+			total = total + passed;
 			passed = 0.0f;
 		}
     }
-	
+    
+    	public void AddToTrace(){
+		string name = gameObject.name;
+		if(Array.IndexOf(trace, name) == -1){
+			trace[index] = name;
+			index = index + 1;
+			points = points + index;
+			Debug.Log(name + ":" + index);
+		}
+	}
+
 	public void DirtBall(){
-		AddForce(600f);
+		if (total < limit){
+			AddToTrace();
+			AddForce(600f);
+		}
 	}
 
 	public void CopperBall(){
-		AddForce(700f);
+		if (total < limit){
+			AddToTrace();
+			AddForce(700f);
+		}
 	}
 
 	public void MarbleBall(){
-		AddForce(100f);
+		if (total < limit){
+			AddToTrace();
+			AddForce(100f);	
+		}
 	}
 
 	public void SunBall(){
-		Destroy(gameObject);
+		if (total < limit){
+			AddToTrace();
+			points = points * 2;
+			AudioSource audioData = GetComponent<AudioSource>();
+			audioData.Play(0);
+			Destroy(gameObject);
+		}
 	}
 
 	public void TennisBall(){
-		AddForce(200f);
+		if (total < limit){
+			AddToTrace();
+			AddForce(200f);
+		}
 	}
 	
 	public void VolleyBall(){
-		AddForce(300f);		
+		if (total < limit){
+			AddToTrace();
+			AddForce(300f);	
+		}
 	}
 
 	public void WaterBall(){
-		AddForce(400f);		
+		if (total < limit){
+			AddToTrace();
+			AddForce(400f);		
+		}
 	}
 
 	public void SoccerBall(){
-		AddForce(500f);		
+		if (total < limit){
+			AddToTrace();
+			AddForce(500f);
+		}
 	}
-
 }
